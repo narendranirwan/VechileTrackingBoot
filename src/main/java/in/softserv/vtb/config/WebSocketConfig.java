959705ba34cd08e2d84ca5434b1988071575c23f
Narendra Singh Nirwan <narennirwan@gmail.com>
2023-10-11 14:34:31 +0530

    VehicleTrackingBoot Workspace

---
+++ src/main/java/in/softserv/vtb/config/WebSocketConfig.java
@@ -0,0 +1,31 @@
+package in.softserv.vtb.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
+	
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry config) {
+		config.enableSimpleBroker("/topic");
+		config.setApplicationDestinationPrefixes("/app");
+		//config.setUserDestinationPrefix("/user");
+		
+		//config.setApplicationDestinationPrefixes("/ws");
+	}
+
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/updateMapLocation");
+		registry.addEndpoint("/updateMapLocation").withSockJS();
+		
+		//registry.addEndpoint("/updateMapLocation").setHandshakeHandler(new UserHandshakeHandler()).withSockJS();
+		
+	}
+
+}
